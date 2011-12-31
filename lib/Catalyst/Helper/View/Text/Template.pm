2ee88200b33f093773056dc49297c58fdf5d5539
Dean Hamstead <dean@fragfest.com.au>
2011-12-31 21:03:24 +1100

    make the helper more helpful

--- lib/Catalyst/Helper/View/Text/Template.pm
+++ lib/Catalyst/Helper/View/Text/Template.pm
@@ -53,7 +53,64 @@ use warnings;
 
 use parent 'Catalyst::View::Text::Template';
 
-__PACKAGE__->config(TEMPLATE_EXTENSION => '.tmpl');
+# URI Escaping, uncomment below and in the HASH config
+# use URI::Escape qw( uri_escape );
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tmpl',
+
+# these are added to the HASH *before* the stash is
+# this is a good place to insert your helper functions
+#    HASH        => {
+#
+# add this helper by uncommenting the line below
+#        escapehtml     => \&_escapeHTML,
+#
+# add this helper by uncommenting the 'use URI::Escape' and the line below
+#        'uri_escape'   => \&uri_escape, 
+#
+#}, 
+
+# these are all as per Text::Template documentation
+#    BROKEN      => \&broken_function,
+#    BROKEN_ARGS => \$broken_arg,
+#    DELIMITERS  => [ '[@--', '--@]' ],
+#    PREPEND     => q|use strict; etc...|,
+#    SAFE        => \$safe_cpt,
+
+);
+
+=head2 _escapeHTML
+
+Takes ( $text_to_be_escaped, $escape_whitespace_if_true ).
+I<$text_to_be_escaped> is the actual text, the second option
+I<$escape_whitespace_if_true> causes whitespace to be escaped
+if true. I.e. endlines become br and so forth.
+
+Returns the escaped text;
+
+Lifted from the L<CGI> module.
+
+=cut
+
+sub _escapeHTML {
+
+    my ( $escape, $text ) = @_;
+    return undef unless defined $escape;
+    $escape =~ s/&/&amp;/g;
+    $escape =~ s/"/&quot;/g;
+    $escape =~ s/</&lt;/g;
+    $escape =~ s/>/&gt;/g;
+
+    # these next optional escapes make text look the same when rendered in HTML
+    if ($text) {
+        $escape =~ s/\t/    /g;                          # tabs to 4 spaces
+        $escape =~ s/( {2,})/"&nbsp;" x length $1/eg;    # whitespace escapes
+        $escape =~ s/\n/<br \/>\n/g;                     # newlines to <br />
+    }
+    return $escape;
+
+}
 
 =head1 NAME
 
